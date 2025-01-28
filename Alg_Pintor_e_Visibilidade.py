6716771abf1899eaf7e034dd68fda795ddcf52b3
Lens <lensrambinho21@gmail.com>
2025-01-28 18:49:18 -0300

    Código do Pintor Lens concluido

--- Alg_Pintor_e_Visibilidade.py
+++ Alg_Pintor_e_Visibilidade.py
@@ -8,7 +8,7 @@ class PintoreVisibilidade:
         self.VRP = VRP
 
     
-    def calcular_centroide_face(self, vertices, indice_face):
+    def Calcular_centroide_face(self, vertices, indice_face):
 
         v = len(indice_face) # Nº de vertices da face, ex: face ABE = 3; face ABCD = 4;
         
@@ -17,13 +17,12 @@ class PintoreVisibilidade:
         soma_z = 0
 
         for i in indice_face:
-           
             soma_x += vertices[0][i]
             soma_y += vertices[1][i] #vai somando os valores xyz de cada face
             soma_z += vertices[2][i]
 
         centroide_x = soma_x / v
-        centroide_y = soma_y / v #finalmente calcula o centroide, dividindo pelo Nº de vertices
+        centroide_y = soma_y / v #calcula o centroide, dividindo pelo Nº de vertices
         centroide_z = soma_z / v
     
         return centroide_x, centroide_y, centroide_z
@@ -35,6 +34,19 @@ class PintoreVisibilidade:
 
         return dist
 
+    def Calcular_dists_e_Ordenar_faces(self, vertices, VRP, indices_faces):
+
+        face_e_distancia = [] #face e distancia dela em relação ao VRP
+
+        for indice_face in indices_faces:
+            centroide = self.Calcular_centroide_face(vertices, indice_face)
+            distancia = self.Calcular_distancia_VRP_Face(VRP, centroide)
+            face_e_distancia.append((distancia, indice_face))
+
+        #Ordenar as faces pela dist, maior para menor
+        Faces_ordenadas = sorted(face_e_distancia, key=lambda x: x[0], reverse=True)
+
+        return Faces_ordenadas
 
 
 if __name__ == "__main__":
@@ -44,14 +56,16 @@ if __name__ == "__main__":
                 [42.3, 27.2, 14.6, 29.7,31.6],
                 [  1,   1 ,   1,     1,  1]]
     
-    indice_face = [1,2,4]  #Os vertices de cada face, ex: Face ABE(Face 014)
+    indices_faces = [ [0,1,4],[1,2,4],[2,3,4],[3,0,4],[0,3,2,1] ] #Os vertices de cada face, ex: Face ABE(Face 014)
         
     VRP = [25, 15, 80, 1]   
 
     pintor = PintoreVisibilidade(vertices, VRP) #instancia da classe, só funfa assim
 
-    centroide = pintor.calcular_centroide_face(vertices, indice_face)
-    distancia = pintor.Calcular_distancia_VRP_Face(VRP, centroide)
+    Faces_ordenadas = pintor.Calcular_dists_e_Ordenar_faces(vertices, VRP, indices_faces)
 
-    print(f"\nCentroide da face: {centroide}\n") 
-    print(f"Distância da Face até o VRP: {distancia}\n")
+    print("\nFaces ordenadas da mais distante para a mais próxima:\n") #Essa é a ordem pra ser pintada já, de tras pra frente
+    for distancia, face in Faces_ordenadas:
+        print(f"Face: {face} ; Distância: {distancia:.5f}")
+
+    print("\n")
