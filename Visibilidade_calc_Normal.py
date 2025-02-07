a0ffc98a9d66b8b0920abae03cb9ee1ae0176c00
Lens <lensrambinho21@gmail.com>
2025-02-06 21:05:17 -0300

    Fazendo visibilidade, com erro ainda

--- Visibilidade_calc_Normal.py
+++ Visibilidade_calc_Normal.py
@@ -6,8 +6,34 @@ class Visibilidade_Normal:
     def __init__(self, vertices, indices_faces):
         self.vertices = vertices
         self.indices_faces = indices_faces
+        self.VRP = VRP
+ 
+    def Calcular_vet_normal_unitario_face(self, vertices, indice_face):
 
+        vertices = np.array(vertices) #só funfa convertendo pra array pra manipular
+
+        p0 = vertices[:3, indice_face[0]]
+        p1 = vertices[:3, indice_face[1]] #pegando os vertices da face e seus x,y,z
+        p2 = vertices[:3, indice_face[2]]
+
+        v1 = p1 - p0 
+        v2 = p2 - p0  #vets 1 e 2 para calcular o vet normal
+
+        Vnormal = np.cross(v1, v2)  #produto vetorial v1 x v2
+  
+        norma = np.linalg.norm(Vnormal)
+        Vnormal_unitario = Vnormal / norma  #calc do vet unitario
+
+        return Vnormal_unitario
     
+    def Calcular_vet_normal_das_faces(self, vertices, indices_faces):
+       
+        print("\n")
+        for i, face in enumerate(indices_faces[:4]):  #só funfa para faces com 3 vertices por enquanto
+            normal_unitaria = visi.Calcular_vet_normal_unitario_face(vertices, face)
+            print(f"Normal da face {i}: {normal_unitaria}")
+        print("\n")
+
     def Calcular_centroide_face(self, vertices, indice_face):
 
         v = len(indice_face) # Nº de vertices da face, ex: face ABE = 3; face ABCD = 4;
@@ -27,23 +53,6 @@ class Visibilidade_Normal:
     
         return centroide_x, centroide_y, centroide_z
     
-
-
-    def Calcular_vet_normal_unitario_face(self, vertices, indice_faces):
-
-        p0 = vertices[:, indice_faces[0]]
-        p1 = vertices[:, indice_faces[1]]  #pegando as coordenadas x,y,z de cada vertice
-        p2 = vertices[:, indice_faces[2]]
-
-        v1 = p2 - p1   #calculo dos vetores 1 e 2 para calcular o vetor normal
-        v2 = p0 - p1
-
-        Vnormal = np.cross(v1, v2)  #produto vetorial v1 x v2
-  
-        Vnormal_unitario = Vnormal / np.linalg.norm(Vnormal)  #calc do vetor unitario
-
-        return Vnormal_unitario
-    
     
 if __name__ == "__main__":
    
@@ -53,12 +62,12 @@ if __name__ == "__main__":
                 [  1,   1 ,   1,     1,  1]]
     
     indices_faces = [ [0,1,4],[1,2,4],[2,3,4],[3,0,4],[0,3,2,1] ] #Os vertices de cada face, ex: Face ABE(Face 014)
-        
+   
+    indice_face = [[0,1,4]]    
+
     VRP = [25, 15, 80, 1]   
 
+    visi = Visibilidade_Normal(vertices, indice_face) #instancia da classe, só funfa assim
 
-    visi = Visibilidade_Normal(vertices, indices_faces) #instancia da classe, só funfa assim
-
-    normal_014 = visi.Calcular_vet_normal_unitario_face(vertices, indices_faces)
-
+    visi.Calcular_vet_normal_das_faces(vertices, indices_faces)
     
