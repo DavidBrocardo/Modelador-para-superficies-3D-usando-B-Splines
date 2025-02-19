6fa352c58e8e8928acd9a1bd7f882eca8dff8da8
davidbrocardo@gmail.com <70301267+DavidBrocardo@users.noreply.github.com>
2025-02-19 11:03:50 -0300

    Interface alterando visualizacao em tempo de execucao

--- Interface_BSplines.py
+++ Interface_BSplines.py
@@ -31,6 +31,7 @@ class Interface:
 
         # Criar interface
         self.criar_menu()
+        self.criar_botoes()
         self.main()
 
     def criar_menu(self):
@@ -40,10 +41,10 @@ class Interface:
         # Menu Superfice
         menu_superfice = tk.Menu(menu_principal, tearoff=0)
         menu_superfice.add_command(label="Pontos de Controle", command=self.definir_ponto_controle)
-        menu_superfice.add_command(label="Tamanho da Matrix", command=self.definir_tamanho_matriz)
-        menu_superfice.add_command(label="Camera", command=self.salvar_arquivo)
-        menu_superfice.add_command(label="Viewport", command=self.salvar_arquivo)
-        menu_superfice.add_command(label="Windows", command=self.salvar_arquivo)
+        menu_superfice.add_command(label="Ponto Focal", command=self.ponto_focal)
+        menu_superfice.add_command(label="Camera", command=self.definir_camera)
+        menu_superfice.add_command(label="Viewport", command=self.definir_viewport)
+        menu_superfice.add_command(label="Windows", command=self.definir_windows)
         menu_principal.add_cascade(label="Superfice", menu=menu_superfice)
         
         # Menu Arquivo
@@ -55,15 +56,28 @@ class Interface:
         menu_principal.add_cascade(label="Arquivo", menu=menu_arquivo)
 
         # Menu Pintura
-        menu_editar = tk.Menu(menu_principal, tearoff=0)
-        menu_editar.add_command(label="Copiar")
-        menu_editar.add_command(label="Colar")
-        menu_principal.add_cascade(label="Editar", menu=menu_editar)
+        menu_pintor = tk.Menu(menu_principal, tearoff=0)
+        menu_pintor.add_command(label="Cor Frente")
+        menu_pintor.add_command(label="Cor Fundo")
+        menu_principal.add_cascade(label="Pintura", menu=menu_pintor)
 
         # Menu Sobreamento
 
+        menu_sobra = tk.Menu(menu_principal, tearoff=0)
+        menu_sobra.add_command(label="Sombreamento Constante")
+        menu_sobra.add_command(label="Sombreamento Gouraud")
+        menu_sobra.add_command(label="Sombreamento Phong ")        
+        menu_principal.add_cascade(label="Sombreamento", menu=menu_sobra)
+
         self.tela.config(menu=menu_principal)
-        self.tela.bind_all("<Control-q>", lambda event: self.sair())
+
+    def criar_botoes(self):
+        
+        frame_botoes = tk.Frame(self.tela)
+        frame_botoes.pack(pady=10)
+
+        btn_pontos = tk.Button(frame_botoes, text="Nova Superfice", command=self.definir_tamanho_matriz, width=25, bg="blue")
+        btn_pontos.grid(row=0, column=0, padx=5)
 
     def abrir_arquivo(self):
         messagebox.showinfo("Abrir", "Abrindo arquivo...")
@@ -75,24 +89,9 @@ class Interface:
         self.tela.quit()
 
     def definir_ponto_controle(self):
+        print("Lens Pacu")
+
         
-        janela = tk.Toplevel(self.tela)
-        janela.title("Definir Ponto de Controle")
-
-        tk.Label(janela, text="X:").grid(row=0, column=0)
-        entrada_x = tk.Entry(janela)
-        entrada_x.grid(row=0, column=1)
-
-        tk.Label(janela, text="Y:").grid(row=1, column=0)
-        entrada_y = tk.Entry(janela)
-        entrada_y.grid(row=1, column=1)
-
-        tk.Label(janela, text="Z:").grid(row=2, column=0)
-        entrada_z = tk.Entry(janela)
-        entrada_z.grid(row=2, column=1)
-
-        #tk.Button(janela, text="Salvar", command=salvar_ponto).grid(row=3, columnspan=2)
-
     def definir_tamanho_matriz(self):
         
         janela = tk.Toplevel(self.tela)
@@ -100,36 +99,98 @@ class Interface:
 
         tk.Label(janela, text="Linhas (X):").grid(row=0, column=0)
         entrada_x = tk.Entry(janela)
-        entrada_x.grid(row=0, column=1)
+        entrada_x.grid(row=1, column=0)
 
-        tk.Label(janela, text="Colunas (Y):").grid(row=1, column=0)
+        tk.Label(janela, text="Colunas (Y):").grid(row=0, column=1)
         entrada_y = tk.Entry(janela)
         entrada_y.grid(row=1, column=1)
-
+        
         def Salvar():
-            self.pontos_controleX =  entrada_x
-            self.pontos_controleY =  entrada_y
-            self.criar_pontos_controle()                       
+            self.pontos_controleX = int(entrada_x.get())
+            self.pontos_controleY = int(entrada_y.get())
+
+            print(entrada_x, entrada_y)
+            self.criar_pontos_controle() 
+            janela.destroy()                      
             # Calcula a superfície B-Spline
-            bspline = BSplines(self.pontos_controleX, self.pontos_controleY, self.TI, self.TJ, self.RESOLUTIONI, self.RESOLUTIONJ,
+            bspline = BSplines(self.pontos_controleX,self.pontos_controleY , self.TI, self.TJ, self.RESOLUTIONI, self.RESOLUTIONJ,
                             self.inp, self.VRP, self.P, self.Y, self.dp, self.windows, self.viewport)
-            self.inp, self.outp = bspline.main()
+            self.inp_Axo, self.outp = bspline.main()
             self.desenhar_superficie()
 
         tk.Button(janela, text="Salvar", command=Salvar).grid(row=2, columnspan=2)
 
-    def definir_windows(self):
+    def ponto_focal(self):
         
         janela = tk.Toplevel(self.tela)
-        janela.title("Definir Tamanho da Matriz")
+        janela.title("Definir Novo Ponto Focal")
 
-        tk.Label(janela, text="Linhas (X):").grid(row=0, column=0)
+        tk.Label(janela, text="X:").grid(row=0, column=0)
         entrada_x = tk.Entry(janela)
-        entrada_x.grid(row=0, column=1)
+        entrada_x.grid(row=1, column=0)
 
-        tk.Label(janela, text="Colunas (Y):").grid(row=1, column=0)
+        tk.Label(janela, text="Y").grid(row=0, column=1)
         entrada_y = tk.Entry(janela)
         entrada_y.grid(row=1, column=1)
+
+        tk.Label(janela, text="Z").grid(row=0, column=2)
+        entrada_z = tk.Entry(janela)
+        entrada_z.grid(row=1, column=2)
+        
+        def Salvar():
+            x = float(entrada_x.get())
+            y = float(entrada_y.get())
+            z = float(entrada_z.get())
+            self.P = []
+            self.P =  [x, y , z, 1]
+            
+            janela.destroy()                      
+            # Calcula a superfície B-Spline
+            bspline = BSplines(self.pontos_controleX,self.pontos_controleY , self.TI, self.TJ, self.RESOLUTIONI, self.RESOLUTIONJ,
+                            self.inp, self.VRP, self.P, self.Y, self.dp, self.windows, self.viewport)
+            self.inp_Axo, self.outp = bspline.main()
+            self.desenhar_superficie()
+
+        tk.Button(janela, text="Salvar", command=Salvar).grid(row=2, columnspan=3)
+
+    def definir_windows(self):
+        #windows = [-100, -100, 100, 100]
+        
+        janela = tk.Toplevel(self.tela)
+        janela.title("Definir Nova Viewport")
+
+        tk.Label(janela, text="X min:").grid(row=0, column=0)
+        entrada_xMin = tk.Entry(janela)
+        entrada_xMin.grid(row=1, column=0)
+
+        tk.Label(janela, text="X max").grid(row=0, column=1)
+        entrada_xMax = tk.Entry(janela)
+        entrada_xMax.grid(row=1, column=1)
+
+        tk.Label(janela, text="Y min:").grid(row=0, column=2)
+        entrada_yMin = tk.Entry(janela)
+        entrada_yMin.grid(row=1, column=2)
+
+        tk.Label(janela, text="Y max:").grid(row=0, column=3)
+        entrada_yMax = tk.Entry(janela)
+        entrada_yMax.grid(row=1, column=3)
+        
+        def Salvar():
+            x_min = float(entrada_xMin.get())
+            x_max = float(entrada_xMax.get())
+            y_min = float(entrada_yMin.get())
+            y_max = float(entrada_yMax.get())
+            self.windows = []
+            self.windows =  [x_min, y_min, x_max, y_max]
+            
+            janela.destroy()                      
+            # Calcula a superfície B-Spline
+            bspline = BSplines(self.pontos_controleX,self.pontos_controleY , self.TI, self.TJ, self.RESOLUTIONI, self.RESOLUTIONJ,
+                            self.inp, self.VRP, self.P, self.Y, self.dp, self.windows, self.viewport)
+            self.inp_Axo, self.outp = bspline.main()
+            self.desenhar_superficie()
+
+        tk.Button(janela, text="Salvar", command=Salvar).grid(row=2, columnspan=4)
 
         
 
@@ -137,35 +198,74 @@ class Interface:
     def definir_viewport(self):
         
         janela = tk.Toplevel(self.tela)
-        janela.title("Definir Tamanho da Matriz")
+        janela.title("Definir Nova Viewport")
 
-        tk.Label(janela, text="Linhas (X):").grid(row=0, column=0)
-        entrada_x = tk.Entry(janela)
-        entrada_x.grid(row=0, column=1)
+        tk.Label(janela, text="U min:").grid(row=0, column=0)
+        entrada_uMin = tk.Entry(janela)
+        entrada_uMin.grid(row=1, column=0)
 
-        tk.Label(janela, text="Colunas (Y):").grid(row=1, column=0)
-        entrada_y = tk.Entry(janela)
-        entrada_y.grid(row=1, column=1)
+        tk.Label(janela, text="U max").grid(row=0, column=1)
+        entrada_uMax = tk.Entry(janela)
+        entrada_uMax.grid(row=1, column=1)
 
+        tk.Label(janela, text="V min:").grid(row=0, column=2)
+        entrada_vMin = tk.Entry(janela)
+        entrada_vMin.grid(row=1, column=2)
+
+        tk.Label(janela, text="V max:").grid(row=0, column=3)
+        entrada_vMax = tk.Entry(janela)
+        entrada_vMax.grid(row=1, column=3)
+        
+        def Salvar():
+            u_min = float(entrada_uMin.get())
+            u_max = float(entrada_uMax.get())
+            v_min = float(entrada_vMin.get())
+            v_max = float(entrada_vMax.get())
+            self.viewport = []
+            self.viewport =  [u_min, v_min, u_max, v_max]
+            
+            janela.destroy()                      
+            # Calcula a superfície B-Spline
+            bspline = BSplines(self.pontos_controleX,self.pontos_controleY , self.TI, self.TJ, self.RESOLUTIONI, self.RESOLUTIONJ,
+                            self.inp, self.VRP, self.P, self.Y, self.dp, self.windows, self.viewport)
+            self.inp_Axo, self.outp = bspline.main()
+            self.desenhar_superficie()
+
+        tk.Button(janela, text="Salvar", command=Salvar).grid(row=2, columnspan=4)
 
     def definir_camera(self):
-        
+        #VRP = [0, 0 ,0.7, 1]
+
         janela = tk.Toplevel(self.tela)
-        janela.title("Definir Tamanho da Matriz")
+        janela.title("Definir Nova Camera")
 
-        tk.Label(janela, text="Linhas (X):").grid(row=0, column=0)
+        tk.Label(janela, text="X:").grid(row=0, column=0)
         entrada_x = tk.Entry(janela)
-        entrada_x.grid(row=0, column=1)
+        entrada_x.grid(row=1, column=0)
 
-        tk.Label(janela, text="Colunas (Y):").grid(row=1, column=0)
+        tk.Label(janela, text="Y").grid(row=0, column=1)
         entrada_y = tk.Entry(janela)
         entrada_y.grid(row=1, column=1)
 
+        tk.Label(janela, text="Z").grid(row=0, column=2)
+        entrada_z = tk.Entry(janela)
+        entrada_z.grid(row=1, column=2)
+        
+        def Salvar():
+            x = float(entrada_x.get())
+            y = float(entrada_y.get())
+            z = float(entrada_z.get())
+            self.VRP = []
+            self.VRP =  [x, y , z, 1]
+            
+            janela.destroy()                      
+            # Calcula a superfície B-Spline
+            bspline = BSplines(self.pontos_controleX,self.pontos_controleY , self.TI, self.TJ, self.RESOLUTIONI, self.RESOLUTIONJ,
+                            self.inp, self.VRP, self.P, self.Y, self.dp, self.windows, self.viewport)
+            self.inp_Axo, self.outp = bspline.main()
+            self.desenhar_superficie()
 
-
-
-
-
+        tk.Button(janela, text="Salvar", command=Salvar).grid(row=2, columnspan=3)
 
 
     def criar_pontos_controle(self):
@@ -183,23 +283,24 @@ class Interface:
     def desenhar_superficie(self):
         """Desenha a superfície B-Spline no Canvas."""
         self.canvas.delete("all")
-        escala = 1
-        deslocamento_x, deslocamento_y = 50, 250  # Ajuste para centralizar
+         
+        #deslocamento_x,  50, 250  # Ajuste para centralizar
 
         # Desenha os pontos de controle
         for i in range(self.pontos_controleX + 1):
             for j in range(self.pontos_controleY + 1):
-                x = deslocamento_x + self.inp[i][j][0] * escala
-                y = deslocamento_y - self.inp[i][j][2] * escala
+                x = self.inp_Axo[i][j][0]
+                y = self.inp_Axo[i][j][1] 
+                #print (x ,  y)
                 self.canvas.create_oval(x-3, y-3, x+3, y+3, fill="red")
 
         # Desenha a superfície usando linhas
         for i in range(self.RESOLUTIONI - 1):
             for j in range(self.RESOLUTIONJ - 1):
-                x1, y1 = deslocamento_x + self.outp[i][j][0] * escala, deslocamento_y - self.outp[i][j][2] * escala
-                x2, y2 = deslocamento_x + self.outp[i][j+1][0] * escala, deslocamento_y - self.outp[i][j+1][2] * escala
-                x3, y3 = deslocamento_x + self.outp[i+1][j+1][0] * escala, deslocamento_y - self.outp[i+1][j+1][2] * escala
-                x4, y4 = deslocamento_x + self.outp[i+1][j][0] * escala, deslocamento_y - self.outp[i+1][j][2] * escala
+                x1, y1 =   self.outp[i][j][0] ,   self.outp[i][j][1] 
+                x2, y2 =  self.outp[i][j+1][0] ,  self.outp[i][j+1][1] 
+                x3, y3 =  self.outp[i+1][j+1][0] ,  self.outp[i+1][j+1][1]
+                x4, y4 =   self.outp[i+1][j][0] ,  self.outp[i+1][j][1] 
                 
                 self.canvas.create_line(x1, y1, x2, y2, fill="black")
                 self.canvas.create_line(x2, y2, x3, y3, fill="black")
@@ -209,14 +310,14 @@ class Interface:
     def main(self):
         """Executa os cálculos e desenha a superfície."""
         self.criar_pontos_controle()
-        
+        self.inp_Axo = []
         # Calcula a superfície B-Spline
         bspline = BSplines(self.pontos_controleX, self.pontos_controleY, self.TI, self.TJ, self.RESOLUTIONI, self.RESOLUTIONJ,
                            self.inp, self.VRP, self.P, self.Y, self.dp, self.windows, self.viewport)
-        self.inp, self.outp = bspline.main()
+        self.inp_Axo, self.outp = bspline.main()
         
-        print("\n\nPontos da superfície:", self.outp)
-        print("\n\nPontos de controle:", self.inp)
+        #print("\n\nPontos da superfície:", self.outp)
+        #print("\n\nPontos de controle:", self.inp)
 
         self.desenhar_superficie()
 
@@ -225,9 +326,9 @@ if __name__ == "__main__":
     pontos_controleX, pontos_controleY = 5, 5  
     TI, TJ = 4, 4  
     RESOLUTIONI, RESOLUTIONJ = 10, 10  
-    espacamento = 20
-    VRP = [10, 10, 10, 1]
-    P = [0, 0, 0, 1]
+    espacamento = 14
+    VRP = [0, 0 ,0.7, 1]
+    P = [1, 2, 2, 1]
     Y = [0, 1, 0]
     dp = 40
     windows = [-100, -100, 100, 100]
